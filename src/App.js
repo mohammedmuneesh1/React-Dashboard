a1f14e16a800db4c1e50ae5d1e981adf94217313
Mohammed Muneesh E K <mohammedmuneesh1@gmail.com>
2024-02-07 20:58:18 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
+import { About } from './pages/About';
+import { Comment } from './pages/Comment';
+import { Product } from './pages/Product';
+import { ProductList } from './pages/ProductList';
+import { Sidebar } from './components/Sidebar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Sidebar>
+    <Routes>
+      <Route path="/" element={<Dashboard/> }/>
+      <Route path="/dashboard" element={<Dashboard/> }/>
+      <Route path="/about" element={<About/>} />
+      <Route path="/comment" element={<Comment/>} />
+      <Route path="/analytics" element={<About/>} />
+      <Route path="/product" element={<Product/>} />
+      <Route path="/productlist" element={<ProductList/>} />
+    </Routes>
+    </Sidebar>
+    </>
   );
 }
 
